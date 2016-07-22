387e4391749d4fb8786688f856fc976077be63dc
131220070-Zhou Yaoyang <diamondzyy@sina.com>
2016-07-22 10:32:27 +0800

    run script

---
+++ run_gem5_alpha_spec06_benchmark.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+#
+# run_gem5_alpha_spec06_benchmark.sh
+# Author: Mark Gottscho Email: mgottscho@ucla.edu
+# Copyright (C) 2014 Mark Gottscho
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+############ DIRECTORY VARIABLES: MODIFY ACCORDINGLY #############
+# Install location of gem5
+# GEM5_DIR=/home/zhouyaoyang/projects/gem5-stable
+GEM5_DIR=$PWD
+# Install location of your SPEC2006 benchmarks
+SPEC_DIR=/home/zhouyaoyang/cpu2006
+##################################################################
+
+# Get command line input. We will need to check these.
+BENCHMARK=$1                    # Benchmark name, e.g. bzip2
+OUTPUT_DIR=$2                   # Directory to place run output.
+SMT=$3
+
+SCRIPT_OUT=$OUTPUT_DIR/runscript.log
+# File log for this script's stdout henceforth
+
+################## REPORT SCRIPT CONFIGURATION ###################
+
+echo "Command line:"                                | tee $SCRIPT_OUT
+echo "$0 $*"                                        | tee -a $SCRIPT_OUT
+echo "================= Hardcoded directories ==================" | tee -a $SCRIPT_OUT
+echo "GEM5_DIR:                                     $GEM5_DIR" | tee -a $SCRIPT_OUT
+echo "SPEC_DIR:                                     $SPEC_DIR" | tee -a $SCRIPT_OUT
+echo "==================== Script inputs =======================" | tee -a $SCRIPT_OUT
+echo "BENCHMARK:                                    $BENCHMARK" | tee -a $SCRIPT_OUT
+echo "OUTPUT_DIR:                                   $OUTPUT_DIR" | tee -a $SCRIPT_OUT
+echo "==========================================================" | tee -a $SCRIPT_OUT
+##################################################################
+
+
+#################### LAUNCH GEM5 SIMULATION ######################
+echo ""
+
+echo "" | tee -a $SCRIPT_OUT
+echo "" | tee -a $SCRIPT_OUT
+echo "--------- Here goes nothing! Starting gem5! ------------" | tee -a $SCRIPT_OUT
+echo "" | tee -a $SCRIPT_OUT
+echo "" | tee -a $SCRIPT_OUT
+
+################# detailed
+$GEM5_DIR/build/ALPHA/gem5.opt\
+    --outdir=$OUTPUT_DIR $GEM5_DIR/configs/spec/spec06_config.py\
+    --benchmark="$BENCHMARK"\
+    --benchmark_stdout=$OUTPUT_DIR/$BENCHMARK.out\
+    --benchmark_stderr=$OUTPUT_DIR/$BENCHMARK.err\
+    --mem-size=4GB\
+    --cpu-type=detailed\
+    --caches\
+    $SMT\
+    | tee -a $SCRIPT_OUT
